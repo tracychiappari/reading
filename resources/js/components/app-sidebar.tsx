af58fd45165e600af06d5d77e0dc64c8a187b787
Tracy Chiappari <tracy.chiappari@gmail.com>
2025-06-19 12:44:38 -0700

    Added a dropdown for the user settings and logout.

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -1,14 +1,46 @@
+import { usePage } from '@inertiajs/react';
+import { type SharedData } from '@/types';
+
 import {
     Sidebar,
     SidebarBody,
+    SidebarFooter,
     SidebarHeader,
-    SidebarHeading,
     SidebarItem,
     SidebarSection
 } from "@/components/ui/sidebar";
-import {BookOpenIcon} from "@heroicons/react/16/solid";
+import {
+    Dropdown,
+    DropdownButton,
+    DropdownMenu,
+    DropdownItem,
+    DropdownLabel,
+    DropdownDivider
+} from "@/components/ui/dropdown";
+import {
+    ArrowRightStartOnRectangleIcon,
+    BookOpenIcon,
+    ChevronUpIcon,
+    Cog8ToothIcon
+} from '@heroicons/react/16/solid';
+import {Avatar} from "@/components/ui/avatar";
+
+const getInitials = (fullName) => {
+    const allNames = fullName.trim().split(' ');
+    const initials = allNames.reduce((acc, curr, index) => {
+        if(index === 0 || index === allNames.length - 1){
+            acc = `${acc}${curr.charAt(0).toUpperCase()}`;
+        }
+        return acc;
+    }, '');
+    return initials;
+}
 
 export function AppSidebar(props: { onClick: () => void }) {
+    const { auth } = usePage<SharedData>().props;
+
+    const initials = getInitials(auth.user.name);
+
     return (
         <Sidebar>
             <SidebarHeader>
@@ -16,15 +48,38 @@ export function AppSidebar(props: { onClick: () => void }) {
             </SidebarHeader>
             <SidebarBody>
                 <SidebarSection>
-                    <SidebarItem href={route("home")}>Home</SidebarItem>
                     <SidebarItem href={route("dashboard")}>Dashboard</SidebarItem>
                 </SidebarSection>
-                <SidebarSection>
-                    <SidebarHeading>User</SidebarHeading>
-                    <SidebarItem href={route("profile.edit")}>Profile</SidebarItem>
-                    <SidebarItem method="post" href={route("logout")} onClick={props.onClick}>Logout</SidebarItem>
-                </SidebarSection>
             </SidebarBody>
+            <SidebarFooter>
+                <SidebarSection>
+                    <Dropdown>
+                        <DropdownButton as={SidebarItem}>
+                            <span className="flex min-w-0 items-center gap-3">
+                                <Avatar initials={initials} className="size-10" alt=""/>
+                                <span className="min-w-0">
+                                    <span className="block truncate text-sm/5 font-medium text-zinc-950 dark:text-white">{auth.user.name}</span>
+                                    <span className="block truncate text-xs/5 font-normal text-zinc-500 dark:text-zinc-400">
+                                      {auth.user.email}
+                                    </span>
+                                </span>
+                            </span>
+                            <ChevronUpIcon/>
+                        </DropdownButton>
+                        <DropdownMenu className="min-w-64" anchor="top start">
+                            <DropdownItem href={route("profile.edit")}>
+                                <Cog8ToothIcon/>
+                                <DropdownLabel>Settings</DropdownLabel>
+                            </DropdownItem>
+                            <DropdownDivider/>
+                            <DropdownItem method="post" href={route("logout")} onClick={props.onClick}>
+                                <ArrowRightStartOnRectangleIcon/>
+                                <DropdownLabel>Sign out</DropdownLabel>
+                            </DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
+                </SidebarSection>
+            </SidebarFooter>
         </Sidebar>
     );
 }
