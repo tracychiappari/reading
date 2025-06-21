11050f44df1733bd0908cb10ec226dedf41d7777
Tracy Chiappari <tracy.chiappari@gmail.com>
2025-06-20 23:10:12 -0700

    Added dashboard header.

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,8 +1,10 @@
 import {AppLayout} from "@/layouts/app-layout";
+import {Head} from "@inertiajs/react";
 
 export default function Dashboard() {
     return (
         <AppLayout>
+            <Head title="Dashboard"/>
             <div className="max-w-6xl">Contents</div>
         </AppLayout>
     )
