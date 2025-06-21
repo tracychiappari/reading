baed21f3feef156227bfa9bb40a0ea03f218d538
Tracy Chiappari <tracy.chiappari@gmail.com>
2025-06-21 02:22:35 -0700

    Added more book details and formatting.

--- resources/js/pages/books/show.tsx
+++ resources/js/pages/books/show.tsx
@@ -25,18 +25,23 @@ export default function Show({ book }: Props) {
                     <Heading>{book.title}</Heading>
                     <Button color="indigo" href={route('books.edit', {id: book.id})}>Edit Book</Button>
                 </div>
-                <div>
+                <div className="flex justify-start items-start">
+                    <div className="p-6 pl-0">
                     {book.cover && (
                         <img
                             src={`/${book.cover}`}
                             alt={`Cover of ${book.title}`}
-                            className="h-32 w-auto object-cover"
+                            className="h-80 w-auto object-cover"
                             onError={(e) => {
                                 (e.target as HTMLImageElement).parentElement!.innerHTML =
                                     '<div class="h-32 w-24 bg-gray-200 flex items-center justify-center"><span class="text-gray-400">No Cover</span></div>';
                             }}
                         />
                     )}
+                    </div>
+                    <div className="p-6">
+                        {book.title} | {book.author}
+                    </div>
                 </div>
             </div>
         </AppLayout>
