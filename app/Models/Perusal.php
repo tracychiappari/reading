1ec1457f3c381f0a8bd4e4a6451c3131ef4ca0d2
Tracy Chiappari <tracy.chiappari@gmail.com>
2025-07-09 13:52:28 -0700

    Updated perusals to include scope functions

--- app/Models/Perusal.php
+++ app/Models/Perusal.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Attributes\Scope;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 
@@ -32,5 +34,29 @@ class Perusal extends Model
     public function user(): HasOneThrough
     {
         return $this->hasOneThrough(User::class, Book::class);
+    }
+
+    #[Scope]
+    public function pending(Builder $builder): void
+    {
+        $builder->where('status', 'not_started');
+    }
+
+    #[Scope]
+    public function reading(Builder $builder): void
+    {
+        $builder->where('status', 'in_progress');
+    }
+
+    #[Scope]
+    public function completed(Builder $builder): void
+    {
+        $builder->where('status', 'completed');
+    }
+
+    #[Scope]
+    public function abandoned(Builder $builder): void
+    {
+        $builder->where('status', 'abandoned');
     }
 }
